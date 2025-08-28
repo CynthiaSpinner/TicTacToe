fdd25ff4ead45c769fc9953f48544c0c8ccd7dba
Cynthia Spinner <CynthiaSpinner@gmail.com>
2025-08-28 12:16:38 -0500

    added player 1 and player 2, and start game, and basic testing javascript for gameboard

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-// Add your JavaScript code here
+const GameBoard = (() =>{
+    let board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']; //test
 
-console.log('Tic Tac Toe game loaded!');
+    const render = () => {
+        const gameboardDiv = document.getElementById('gameboard');
+        gameboardDiv.innerHTML = '';
+
+        board.forEach((cell, index) => {
+            const cellDiv = document.createElement('div');
+            cellDiv.classList.add('cell');
+            cellDiv.textContent =cell;
+            cellDiv.dataset.index = index;
+            gameboardDiv.appendChild(cellDiv);
+        });
+    };
+
+    return { render };
+})();
+
+//testing
+GameBoard.render();
+
+
